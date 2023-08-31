5deefd21e0ebe540f43387806cd73a66115031d5
Mike Koenders <mike.koenders29@gmail.com>
2023-08-31 23:46:11 +0200

    second  commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-This project was build with the help of Colt Steele and his Web Development Course on Udemy
+This project was created with the help of Colt Steele and his web development course on Udemy.
+
+For learning purposes only.
--- cloudinary/index.js
+++ cloudinary/index.js
@@ -1,35 +1,26 @@
-const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
-//Cloudinary für unsere App einstellen und konfigurieren
-//Mit den API KEys etc. von cloudinary in der .env Datei
-/* cloudinary.config({ 
-    cloud_name: 'dd6ahivgd', 
-    api_key: '514925723425896', 
-    api_secret: 'UtG0h4NOg-iY9Ee7-ipCKwTDGSE' 
-  });
- */  
-
-  cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET
-  })
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
 //Instanzieren eine Instanz von CloudinaryStorage
 const storage = new CloudinaryStorage({
-    //Objekt von oben einfügen
-    cloudinary: cloudinary,
-    params: {
-        //FOlder für Cloudinary wo wir die Dateien speichern
-        folder: 'Studios',
-        //welche Foramte erlaubt sind
-        allowedFormats: ['jpeg', 'png', 'jpg']    
-    }
+  //Objekt von oben einfügen
+  cloudinary: cloudinary,
+  params: {
+    //FOlder für Cloudinary wo wir die Dateien speichern
+    folder: "Studios",
+    //welche Foramte erlaubt sind
+    allowedFormats: ["jpeg", "png", "jpg"],
+  },
 });
 
 //Damit man das exportieren kann
 //die Instanzen Cloudinary und storage
 module.exports = {
-    cloudinary,
-    storage
-}
+  cloudinary,
+  storage,
+};
